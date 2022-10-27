49594c99dbcf35eaf5ee8330fb7c2011f8ceb848
dhru prajapati <dhruprajapati18@gmail.com>
2022-10-27 12:37:53 +0530

    Micro weber

---
+++ main.tf
@@ -0,0 +1,37 @@
+ terraform {
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+       version = "~> 4.0"
+     }
+   }
+ }
+
+
+
+resource "aws_apprunner_service" "example" {
+
+  service_name = "example"
+
+  source_configuration {
+    authentication_configuration{
+      access_role_arn=  "arn:aws:iam::530211691714:role/micro-web"
+}
+    
+    image_repository {
+      image_configuration {
+        port = "80"
+      }
+      image_identifier      = "530211691714.dkr.ecr.ap-northeast-1.amazonaws.com/microweb:latest"
+      image_repository_type = "ECR"
+    }
+    auto_deployments_enabled = false
+    
+ 
+  }
+
+  tags = {
+    Name = "example-apprunner-service"
+  }
+}
+
